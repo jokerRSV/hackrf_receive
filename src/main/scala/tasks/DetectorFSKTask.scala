b4b8ea4fcb5ff5cf0ddb3cda93ff9e7d20e9f39e
rsv <sviatoslav.riabinin@gmail.com>
2024-02-19 17:17:56 +0200

    rework slicing

--- src/main/scala/tasks/DetectorFSKTask.scala
+++ src/main/scala/tasks/DetectorFSKTask.scala
@@ -43,25 +43,36 @@ class DetectorFSKTask(startFrequncyHz: Int) extends Runnable {
           .sliding(fskLengthFull, fskLength1kHz.length - 1)
           .toList
           .filter(_.length == fskLengthFull)
-          .par
+          //          .par
           .foreach { l =>
-            val center = levelOne
-            val headFreq = l.head._1
-            val slice = next.curried(l).apply(headFreq)
-            lazy val b0 = slice(0)(20).forall(_._2 >= center)
-            lazy val b4 = slice(3980)(4020).forall(_._2 >= center)
-            lazy val b8 = slice(7980)(8020).forall(_._2 >= center)
-            lazy val b12 = slice(11980)(12000).forall(_._2 >= center)
-            lazy val b2 = slice(1000)(3000).forall(_._2 < center)
-            lazy val b6 = slice(5000)(7000).forall(_._2 < center)
-            lazy val b10 = slice(9000)(11000).forall(_._2 < center)
-            val res = b0 && b4 && b8 && b12 && b2 && b6 && b10
+            //            val center = levelOne
+            //            val headFreq = l.head._1
+            //            val slice = next.curried(l).apply(headFreq)
+            //            lazy val b0 = slice(0)(20).forall(_._2 >= center)
+            //            lazy val b4 = slice(3980)(4020).forall(_._2 >= center)
+            //            lazy val b8 = slice(7980)(8020).forall(_._2 >= center)
+            //            lazy val b12 = slice(11980)(12000).forall(_._2 >= center)
+            //            lazy val b2 = slice(1000)(3000).forall(_._2 < center)
+            //            lazy val b6 = slice(5000)(7000).forall(_._2 < center)
+            //            lazy val b10 = slice(9000)(11000).forall(_._2 < center)
+            val taskList = List(
+              PairsBool(0, 20, condition = true),
+              PairsBool(3980, 4020, condition = true),
+              PairsBool(7980, 8020, condition = true),
+              PairsBool(11980, 12000, condition = true),
+              PairsBool(1000, 3000, condition = false),
+              PairsBool(5000, 7000, condition = false),
+              PairsBool(9000, 11000, condition = false),
+            )
+            //            val res = b0 && b4 && b8 && b12 && b2 && b6 && b10
+            val res = sliceFind(taskList, l)
             if (res) {
-              println(s"start freq: ${l.head._1} center lvl: ${center}_____b0: ${b0}__b4: ${b4}__b8: ${b8}__b12: ${b12}______b2: ${b2}__b6: ${b6}__b10: ${b10}")
+              println(s"start freq: ${l.head._1}")
+              // center lvl: ${center}_____b0: ${b0}__b4: ${b4}__b8: ${b8}__b12: ${b12}______b2: ${b2}__b6: ${b6}__b10: ${b10}")
             }
           }
       }
-      //      val endTime  = System.nanoTime()
+      //      val endTime = System.nanoTime()
       //      println(s"diff time: ${Duration(endTime - startTime, NANOSECONDS).toMillis}")
       loop()
     }
@@ -76,4 +87,27 @@ class DetectorFSKTask(startFrequncyHz: Int) extends Runnable {
       println("completing detector task")
     }
   }
+
+  def sliceFind(taskList: List[PairsBool], arr: Array[(Double, Double)]): Boolean = {
+    val headFreq = arr.head._1
+
+    @tailrec
+    def loop(restTaskList: List[PairsBool], restArr: Array[(Double, Double)]): Boolean = {
+      restTaskList match {
+        case ::(head, next) =>
+          val (first, rest) = restArr.dropWhile(_._1 < head.start + headFreq).span(_._1 <= head.end + headFreq)
+          val res = first.forall(_._2 >= levelOne == head.condition)
+          if (res) {
+            loop(next, rest)
+          } else {
+            false
+          }
+        case Nil => false
+      }
+    }
+
+    loop(taskList, arr)
+  }
 }
+
+case class PairsBool(start: Int, end: Int, condition: Boolean)
